87b1f7d3ecdba3999d9d1dc47164f7229df8dc2d
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-10-23 16:19:58 -0700

    Update Programa17.pl

--- Programa17.pl
+++ Programa17.pl
@@ -16,6 +16,9 @@
 %print(a)
 %print(b) 
 % -------- Código en Prolog --------------------
+main :-
+    eliminar_n_esimo(2, [a, b, c, d, e, f, g], R),
+    write('[a, b, c, d, e, f, g] :'), write(R), nl.
 % Caso donde esta vacia la lista, regresa una lista vacia
 eliminar_n_esimo(_, [], []).  
 
